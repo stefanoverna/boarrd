7968ca980681b54fd1ad6a0b04c1a43d69d7049e
Stefano Verna <stefano.verna@gmail.com>
2010-10-17 19:04:43 +0200

    Fix

--- widgets/widgets/input.rb
+++ widgets/widgets/input.rb
@@ -16,12 +16,15 @@ module Widgets
         p.match(/\(5i\)$/)
       }
       if key and param = key.gsub(/\(5i\)$/, '')
-        DateTime.civil(
-          params[param+"(1i)"],
-          params[param+"(2i)"],
-          params[param+"(3i)"],
-          params[param+"(4i)"],
-          params[param+"(5i)"]
+        send(
+          "#{param}=",
+          DateTime.civil(
+            params[param+"(1i)"],
+            params[param+"(2i)"],
+            params[param+"(3i)"],
+            params[param+"(4i)"],
+            params[param+"(5i)"]
+          )
         )
       end
       params.each_pair do |k, v|
