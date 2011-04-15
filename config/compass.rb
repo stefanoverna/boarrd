082cf33148a00b75a03f8a52d8742acef1522772
Stefano Verna <stefano.verna@gmail.com>
2011-04-15 17:57:00 +0200

    Adding Compass-Heroku fixes

--- config/compass.rb
+++ config/compass.rb
@@ -1,11 +1,13 @@
-# This configuration file works with both the Compass command line tool and within Rails.
-# Require any additional compass plugins here.
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
-# Set this to the root of your project when deployed:
+
 http_path = "/"
-css_dir = (environment.to_s == 'development') ? "public/stylesheets/compiled" : "tmp/public/compiled/stylesheets"
-sass_dir = "app/stylesheets"
+
 environment = Compass::AppIntegration::Rails.env
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+if environment == 'production'
+  css_dir = "tmp/stylesheets"
+  sass_dir = "app/views/stylesheets"
+else
+  css_dir = "public/stylesheets"
+  sass_dir = "app/stylesheets"
+end
